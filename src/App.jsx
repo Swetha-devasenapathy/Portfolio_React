53038950277326a97be3a39461a88392135e59b4
Swetha D <111783503+Swetha-devasenapathy@users.noreply.github.com>
2024-04-12 21:55:14 +0530

    Push

---
+++ src/App.jsx
@@ -0,0 +1,190 @@
+import "./App.css";
+import Porfile from "./assets/Photo.png";
+import Facebook from "./assets/facebook.svg";
+import LinkedIn from "./assets/linkedin.svg";
+import Instagram from "./assets/instagram.svg";
+import ArrowDown from "./assets/arrow-down.svg";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [scrolling, setScrolling] = useState(false);
+
+  const onPageScroll = () => {
+    if(window.pageYOffset > 200) {
+      setScrolling(true)
+    } else {
+      setScrolling(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onPageScroll)
+    return() => {
+      window.removeEventListener("scroll", onPageScroll)
+    }
+  }, [])
+
+  return (
+    <div className="max-w-4xl m-auto relative">
+      <header className={`${scrolling ? 'border-b border-gray-900' : ''}  fixed left-0 right-0 top-0 z-20`} id="home">
+        <div className="container m-auto px-4 py-6 max-w-4xl bg-black">
+          <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
+            <div>
+              <h1 className="font-bold text-2xl">Swetha_Portfolio</h1>
+            </div>
+            <div>
+              <ul className="flex gap-4">
+                <li>
+                  <a href="#About" className="text-gray-400 hover:text-white cursor-pointer">
+                    About Me
+                  </a>
+                </li>
+                <li>
+                  <a href="#Skills" className="text-gray-400 hover:text-white cursor-pointer">
+                    Skills
+                  </a>
+                </li>
+                <li>
+                  <a href="#Projects" className="text-gray-400 hover:text-white cursor-pointer">
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a href="#Education" className="text-gray-400 hover:text-white cursor-pointer">
+                    Education
+                  </a>
+                </li>
+                <li>
+                  <a href="#Contact Me" className="text-gray-400 hover:text-white cursor-pointer">
+                  Contact Me
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="relative mt-28">
+        <section>
+          <div className="container m-auto px-4 pt-12 pb-12 sm:pt-20 flex flex-col sm:flex-row gap-6 text-center sm:text-left">
+            <div>
+              <h2 className="font-bold text-4xl">Hello, I'm Swetha D,</h2>
+              <div>
+                <h2 className="font-bold text-4xl mt-1 gradiant-text">Frontend developer</h2>
+              </div>
+              <div>
+                <p className="mt-4 text-gray-400">
+                A Smart, enthusiastic about learning new things and a good communicator. Passionate Front End Web Developer and an optimistic person with a wide vision towards my career and growth.
+                </p>
+                <button className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  Download CV
+                </button>
+              </div>
+            </div>
+            <div className="relative">
+              <div className="after:bg-[url('./large-long.png')] after:bg-contain after:block after:bg-no-repeat after:w-[420px] after:h-[320px] after:absolute after:top-0 after:-left-20 sm:after:-left-40 before:bg-[url('./small.png')] before:bg-contain before:block before:bg-no-repeat before:w-[220px] before:h-[220px] before:absolute before:bottom-0 before:-right-10">
+                <img src={Porfile} />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="py-10" id="skills">
+          <div className="container m-auto px-4">
+            <h2 className="text-2xl font-semibold">Skills</h2>
+            <div className="mt-14">
+              <div>
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">HTML</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">CSS</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">
+                    JavaScript
+                  </h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-full h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">Figma</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[75%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">Git</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[50%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">React</h2>
+                  <p className="text-gray-500">Basic</p>
+                </div>
+                <span className="w-[60%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">React Native</h2>
+                  <p className="text-gray-500">Basic</p>
+                </div>
+                <span className="w-[45%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="container m-auto flex justify-between px-4 py-6">
+          <div>
+            <p className="text-gray-300 text-sm">Copyright @ 2024</p>
+          </div>
+          <div>
+            <ul className="flex gap-4">
+              <li>
+                <a>
+                  <img src={Facebook} className="w-5" />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img src={LinkedIn} className="w-5" />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img src={Instagram} className="w-5" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      {
+        scrolling && (
+          <button className="fixed block right-8 bottom-0 w-24" onClick={() => {
+            window.scrollTo(0,0);
+          }}>
+            <img src={ArrowDown} />
+          </button>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
